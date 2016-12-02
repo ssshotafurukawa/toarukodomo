d9472e1dc219e705f7fcca85a1682016e417fb68
ssshotafurukawa <b1632994@planet.kanazawa-it.ac.jp>
2016-12-02 20:08:22 +0900

    ver2.osero
    Fails to go to third. Displayed in a strange place.

--- osero.c
+++ osero.c
@@ -3,15 +3,21 @@
 
 #define WHITE 1
 #define BLACK 2
+#define BLANK 0
 #define MASME 8
 #define SUC 1
 #define DEF 0
+#define BLANK 0
+
+int vertical[]={-1,0,1,-1,1,-1,0};
+int cross[]={-1,-1,-1,0,0,1,1};
 
 //‰Šú‰»
-void syokika(int bord[][MASME])
+void init(int colt[][MASME])
 {
 	int i,j;
 	
+	//”Õ–Ê‚Ì‰Šú‰»‚Æ”z’u
 	for(i=1;i<=MASME;i++)
 	{
 		for(j=1;j<=MASME;j++)
@@ -19,57 +25,164 @@ void syokika(int bord[][MASME])
 			if(MASME/2==i&&MASME/2==j||MASME/2+1==i&&MASME/2+1==j)
 			{
 				//”’
-				bord[i][j]=1;
+				colt[i][j]=WHITE;
 			}
-			else if(MASME/2+1==i&&MASME/2==j||MASME/2==i||MASME/2+1==j)
+			else if(MASME/2+1==i&&MASME/2==j||MASME/2==i&&MASME/2+1==j)
 			{
 				//•
-				bord[i][j]=2;
+				colt[i][j]=BLACK;
 			}
 			else
 			{
 				//‹ó”’
-				bord[i][j]=0;
+				colt[i][j]=BLANK;
 			}
 		}
 	}
+	show(colt);
 }
-//”»’è
-int hantei(int x,int y,bord[][MASME])
+//”»’è”’
+int checoowhite(int colt[][MASME],int x,int y)
 {
-	while(1)
+	int i,j,k,num=0;
+	
+	for(i=0;i<=6;i++)
 	{
-		
+		if(colt[x+vertical[i]][y+cross[i]]==BLACK)
+		{
+			//printf("%d  %d\n",x+vertical[i],y+cross[i]);
+			for(j=1;j<=6;j++)
+			{
+				if(colt[x+vertical[i]*j][y+cross[i]*j]==WHITE)
+				{
+					//printf("%d   %d\n",x+vertical[i]*j,y+cross[i]*j);
+					for(k=0;colt[x][y]!=colt[x+vertical[i]*(j-k)][y+cross[i]*(j-k)];k++)
+					{
+						colt[x+vertical[i]*(j-k)][y+cross[i]*(j-k)]=WHITE;
+						//printf("%d  %d %d\n",j-k,vertical[i]*j-k,y+cross[i]*j-k);
+					}
+					num+=1;
+				}
+			}
+		}
+	}
+	if(num==1)
+	{
+		colt[x][y]=WHITE;
+		return SUC;
+	}
+	else
+	{
+		return DEF;
+		printf("‚±‚±‚É‚Í‚¨‚¯‚Ü‚¹‚ñ\n");
+	}
+}
+//”»’è•
+int checooblack(int colt[][MASME],int x,int y)
+{
+	int i,j,k,num=0;
+	
+	for(i=0;i<=6;i++)
+	{
+		if(colt[x+vertical[i]][y+cross[i]]==WHITE)
+		{
+			for(j=1;j<=6;j++)
+			{
+				if(colt[x+vertical[i]*j][y+cross[i]*j]==BLACK)
+				{
+					for(k=0;colt[x][y]!=colt[x+vertical[i]*(j-k)][y+cross[i]*(j-k)];k++)
+					{
+						colt[x+vertical[i]*(j-k)][y+cross[i]*(j-k)]=BLACK;
+					}
+					num+=1;
+				}
+			}
+		}
+	}
+	if(num==1)
+	{
+		colt[x][y]=BLACK;
+		return SUC;
+	}
+	else
+	{
+		return DEF;
+		printf("‚±‚±‚É‚Í‚¨‚¯‚Ü‚¹‚ñ\n");
+	}
 }
 //— •Ô‚µˆ—
-int hyozi(int x,int y,bord[][MASME])
+int show(int colt[][MASME])
 {
-	int i;
+	int i,j;
 	
-	for(i=1;i<=MASME*MASME;i++)
+	for(i=1;i<=MASME;i++)
 	{
-		
-
+		printf(" %d",i);
+	}
+	printf("\n");
+	for(i=1;i<=MASME;i++)
+	{
+		printf("%d",i);
+		for(j=1;j<=MASME;j++)
+		{
+			if(colt[i][j]==WHITE)
+			{
+				printf("œ");	//WHITE
+			}
+			else if(colt[i][j]==BLACK)
+			{
+				printf("Z");	//BLACK
+			}
+			else
+			{
+				printf(" ");	//BLANK
+			}
+		}
+		printf("\n");
+	}
+	
+}
+	
 int main(void)
 {
 	
-	int i,num;
-	int bord[MASME][MASME];
+	int i;
+	int numblack=0,numwhite=0;
+	int colt[MASME][MASME];
+	int x,y;
 	
-	syokika(bord);
+	init(colt);
 	
-	for(i=1;i<=MASME*MASME;i++)
+	for(i=1;i<=MASME*MASME/2;i++)
 	{
 		while(1)
 		{
-			scanf("%d%d",&x,&y)
+			scanf("%d%d",&x,&y);
 			//”»’è
-			num=hantei(x,y,bord);
+			numwhite=checoowhite(colt,x,y);
 			
-			if(num==SUC)break;
+			if(numwhite==SUC)
+			{
+				break;
+			}
 		}
+		numwhite=0;
 		//— •Ô‚µˆ—
-		hyozi(x,y);
+		show(colt);
+		while(1)
+		{
+			scanf("%d%d",&x,&y);
+			//”»’è
+			numblack=checooblack(colt,x,y);
+			
+			if(numblack==SUC)
+			{
+				break;
+			}
+		}
+		numblack=0;
+		show(colt);
 	}
-
-
+	
+	return 0;
+}
